5c5a3c170373035e7466462d128238bbdbb52620
Kevin Cox <kevindanielcox@gmail.com>
2020-10-10 19:15:34 -0400

    Modify styles

--- spontaneity-app-vue/src/components/Main.vue
+++ spontaneity-app-vue/src/components/Main.vue
@@ -63,4 +63,28 @@
 </script>
 
 <style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+
+  section {
+    margin: 2rem;
+    border-radius: 12px;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+    padding: 1rem;
+  }
+
+  body {
+    background-color: yellow;
+  }
+
+  #map,
+  #activities,
+  #inputs {
+    background-color: white;
+  }
 </style>
